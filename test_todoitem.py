b422776abef1835b5209717cacdc65354246c597
george <georgefung78@live.com>
2019-10-25 22:56:48 -0700

    fixed syntax error

--- test_todoitem.py
+++ test_todoitem.py
@@ -9,7 +9,6 @@ def new_todoitem():
     return todoitem
 
 def test_new_todoitem():
-    todoitem = new_todoitem()
     assert todoitem.title == 'this is title' 
     assert todoitem.is_done == True
     assert new_user.creation_date == datetime(days=50,seconds=27,microseconds=10,milliseconds=29000,minutes=5,hours=8,weeks=2)
