86a0add4316f70b9ce95ba03472d231fbfb4fc33
george <georgefung78@live.com>
2019-12-02 16:14:32 -0800

    add test for datetime helper

---
+++ app/test_datetime_helper.py
@@ -0,0 +1,10 @@
+import datetime
+import helpers as helper
+
+def test_datetime_helper():
+    date1 = datetime.datetime(2018)
+    date2 = datetime.datetime(2017)
+    
+    assert helper.get_time_elapse(date1) == '1 year ago'
+    assert helper.get_time_elapse(date2) == '2 years ago'
+
--- helpers.py
+++ helpers.py
@@ -6,15 +6,15 @@ def get_time_elapse(d:datetime.datetime):
     if d == None:
         d = datetime.datetime.now()
 
-    if now.year - d.year >= 0 :
-        return str(now.year - d.year) + ' years ago'
-    elif now.month - d.month >= 0:
-        return str(now.month - d.month) + ' months ago'
-    elif now.day - d.day >= 0:
-        return str(now.day - d.day) + ' days ago'
-    elif now.hour - d.hour >= 0:
-        return str(now.hour - d.hour) + ' hours ago'
-    elif now.minute - d.minute >= 0:
-        return str(now.minute - d.minute) + ' mintues ago'
+    if now.year - d.year > 0 :
+        return str(now.year - d.year) + ' year' + 's' if now.year - d.year > 1 else '' + ' ago'
+    elif now.month - d.month > 0:
+        return str(now.month - d.month) + ' month' + 's' if now.month - d.month > 1 else '' +' ago'
+    elif now.day - d.day > 0:
+        return str(now.day - d.day) + ' day' + 's' if now.day - d.day > 1 else '' +'ago'
+    elif now.minute - d.minute > 60:
+        return str(now.hour - d.hour) + ' hour' + 's' if now.hour - d.hour > 1 else '' + 'ago'
+    elif now.minute - d.minute >0:
+        return str(now.minute - d.minute) + ' mintue' + 's' if now.minute - d.minute > 1 else '' +'ago'
     else:
-        return 'Created just now'
+        return ' just now'
