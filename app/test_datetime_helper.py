987c34dd8acd40ff262535428b561bf3da87fc17
george <georgefung78@live.com>
2019-12-02 16:16:53 -0800

    add test for datetime helper

--- app/test_datetime_helper.py
+++ app/test_datetime_helper.py
@@ -2,8 +2,8 @@ import datetime
 import helpers as helper
 
 def test_datetime_helper():
-    date1 = datetime.datetime(2018)
-    date2 = datetime.datetime(2017)
+    date1 = datetime.datetime(2018,1,1)
+    date2 = datetime.datetime(2017,1,1)
     
     assert helper.get_time_elapse(date1) == '1 year ago'
     assert helper.get_time_elapse(date2) == '2 years ago'
